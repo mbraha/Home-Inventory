92c58f539bf02989ce667488e70aa2c6b6aa052a
Michael Braha <mbraha@localhost.localdomain>
2020-03-09 21:54:17 -0700

    go back to using localstorage instead of cookies....for now. Also general cleanup

--- app/static/js/App.jsx
+++ app/static/js/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import RegisterForm from "./comps/Register";
 import LoginForm from "./comps/Login";
+import { response_to_json } from "./utils";
 
 class App extends Component {
   state = {
@@ -11,12 +12,29 @@ class App extends Component {
     refresh_token: null
   };
 
+  async componentDidMount() {
+    const refresh_token = localStorage.getItem("refresh_token");
+    console.log("got token from storage", refresh_token);
+
+    // Try and get a new access token
+    const response = await fetch("http://127.0.0.1:5000/token/refresh", {
+      method: "POST",
+      headers: new Headers({ Authorization: "Bearer " + refresh_token })
+    });
+    const refresher = await response_to_json(response);
+    console.log("App didMount refresher", refresher);
+    if (refresher.hasOwnProperty("access_token")) {
+      this.setState({ isLoggedIn: true, access_token: refresher });
+    }
+  }
+
   setLoggedInStatus = status => {
     if (status.hasOwnProperty("error")) {
       console.log("status has error");
       this.setState({ isLoggedIn: false });
     } else {
       // console.log("status", status);
+      localStorage.setItem("refresh_token", status.refresh_token);
       this.setState({
         isLoggedIn: true,
         access_token: status.access_token,
@@ -32,6 +50,7 @@ class App extends Component {
       <Switch>
         <Route exact path="/">
           <h1>home!</h1>
+          <h2>Are you logged in? {isLoggedIn.toString().toUpperCase()}</h2>
         </Route>
 
         <Route path="/register">
