99f521f17da2b6d9d8ff2547785537d11ebe2e2d
Michael Braha <mbraha@localhost.localdomain>
2020-04-08 18:03:07 -0700

    It now installs itself

---
+++ README.md
@@ -0,0 +1,22 @@
+# Prerequisites
+
+1. Python 3
+   To check, `python3 -V` in a terminal.
+
+2. MongoDB
+   Install it for your OS. [MongoDB Install Instructions](https://docs.mongodb.com/manual/installation/)
+
+   MongoDB must be run at the same MONGO_URI used in `config.py`.
+
+# Install
+
+In the root of the project, run the install script:
+`./install.sh`
+
+# Usage
+
+To run the entire application:
+`./dev.sh`
+
+To stop:
+`./kill.sh`
---
+++ install.sh
@@ -0,0 +1,20 @@
+# Stop script if a command fails
+set -e
+
+# Create python virtual environment and activate it.
+python3 -m venv ./venv || echo "failed to make venv"
+source ./venv/bin/activate || echo "failed to activate venv"
+echo "  Python virtual env created"
+
+# Install python dependencies.
+pip install -r reqs.txt || echo "failed to pip install py reqs"
+echo "  Python dependencies installed"
+
+# Install javascript dependencies.
+cd ./app/static/
+npm install || echo "failed to npm install"
+cd -
+echo "  JS dependencies installed"
+
+echo ""
+echo "  Install complete!"
