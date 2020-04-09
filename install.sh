88f355df899108d310a08c17331ebd4add8a92a0
Michael Braha <mbraha@localhost.localdomain>
2020-04-08 19:59:54 -0700

    pretty install

--- install.sh
+++ install.sh
@@ -4,17 +4,27 @@ set -e
 # Create python virtual environment and activate it.
 python3 -m venv ./venv || echo "failed to make venv"
 source ./venv/bin/activate
+echo "  *********************"
 echo "  Python virtual env created"
+echo "  *********************"
 
 # Install python dependencies.
 pip install -r reqs.txt || echo "failed to pip install py reqs"
+echo "  *********************"
 echo "  Python dependencies installed"
+echo "  *********************"
 
 # Install javascript dependencies.
 cd ./app/static/
 npm install || echo "failed to npm install"
 cd -
+echo "  *********************"
 echo "  JS dependencies installed"
+echo "  *********************"
 
 echo ""
+echo "  *********************"
+echo "  *********************"
 echo "  Install complete!"
+echo "  *********************"
+echo "  *********************"
